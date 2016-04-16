636b37f175bece5194ebe028063a36d8cbce84d8
Richie Verma <richieverma@gmail.com>
2016-04-15 23:52:39 -0400

    Added details for Dashboard. Currently only a list of players in users team are listed along with score and a corresponding team icon

--- air/urls.py
+++ air/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^dashboard',include(admin.site.urls)),
     url(r'^check_login','air.views.check_login'),
     url(r'^$', 'air.views.login'),
+    url(r'dashboard', views.display_dashboard, name='display_dashboard'),
     
 ]
--- air/views.py
+++ air/views.py
@@ -21,3 +21,31 @@ def check_login(request):
             print "fail"
 	    return render(request, 'air/login.html', {})
 
+def display_dashboard(username, request):
+	#username = request.GET['username']
+	#username = request
+	query_string = 'username:\"'+username+'\"'
+	request_params = urllib.urlencode({'q':query_string,'fl':'team','wt': 'json', 'indent': 'true'})
+	req = urllib2.urlopen('http://52.37.29.91:8983/solr/userData/select',request_params)
+	content = req.read()
+	decoded_json_content = json.loads(content.decode())
+	team_players = decoded_json_content["response"]["docs"][0]["team"]
+	
+	query_string = ''
+	for p in team_players: 
+		query_string += 'Player:\"'+p+'\" '
+		print(p)
+
+	request_params = urllib.urlencode({'q':query_string,'fl':'Player Score Team','wt': 'json', 'indent': 'true'})
+	req = urllib2.urlopen('http://52.37.29.91:8983/solr/stats/select',request_params)
+	
+	content = req.read()
+	decoded_json_content = json.loads(content.decode())
+	json_response = decoded_json_content["response"]
+	feed_data = json_response["docs"]
+	print(feed_data[0]["Team"])
+	context = {"data": feed_data}
+	
+
+	return render(request, 'air/display_dashboard.html', context)	    
+
